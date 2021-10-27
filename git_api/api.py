bf30fb91f4587a4344e53422c7c9d37a94240087
nankuF <fireloki87@gmail.com>
2021-10-27 21:12:25 +0300

    added class CheckUpdate

--- git_api/api.py
+++ git_api/api.py
@@ -1,6 +1,5 @@
 import requests
 from environs import Env
-from pprint import pprint
 
 # Считываем переменные окружения с файла '.env'
 env = Env()
@@ -8,8 +7,33 @@ env.read_env('.env')
 username = env('USERNAME')
 token = env('GITHUB_TOKEN')
 
-# смотрим все репозитории юзера и даты их обновления
-repos = requests.get('https://api.github.com/user/repos', auth=(username, token))
-for repo in repos.json():
-    pprint(repo['html_url'])
-    pprint(f'updated at: {repo["updated_at"]}')
+
+class CheckUpdate:
+    start_date = '1970-01-01T00:00:00Z'
+
+    def __init__(self, username, token, update_at=start_date):
+        self.username = username
+        self.token = token
+        self.update_at = update_at
+
+    def get_repo(self):
+        repos = requests.get('https://api.github.com/user/repos',
+                             auth=(self.username, self.token))
+        for repo in repos.json():
+            if repo['name'] == 'Moscow_stations':
+                self.update_at = repo['updated_at']
+
+    def update_repo(self):
+        if self.update_at > self.start_date:
+            print('DOWNLOAD repository...')
+            self.start_date = self.update_at
+        else:
+            print(self.update_at)
+            print('anyone')
+
+
+# создаем инстанс класса с заведомо старым апдейтом
+moscow = CheckUpdate(username=username, token=token)
+moscow.update_repo()  # проверяем что он реально старый
+moscow.get_repo()  # скачиваем инфу по апдейту с репозитория
+moscow.update_repo()  # удостоверяемся что обновилось
